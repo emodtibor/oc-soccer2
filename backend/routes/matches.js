8fbeb76cb8d001a8bfe56a38b3511aebe245a28c
Tibor Emőd <emod.tibor@gmail.com>
2026-01-24 15:14:01 +0100

    Comment out unused routes and related debug logs in matches.js

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -6,16 +6,16 @@ console.log('list type:', typeof ctrl.list);
 router.get("/", asyncHandler(ctrl.list));
 console.log('create type:', typeof ctrl.create);
 router.post("/", asyncHandler(ctrl.create));
-console.log('remove type:', typeof ctrl.remove);
-router.delete("/:id", asyncHandler(ctrl.remove));
-
-console.log('participants type:', typeof ctrl.participants);
-router.get("/:id/participants", asyncHandler(ctrl.participants));
-console.log('setParticipants type:', typeof ctrl.setParticipants);
-router.post("/:id/participants", asyncHandler(ctrl.setParticipants));
-console.log('getTeams type:', typeof ctrl.getTeams);
-router.get("/:id/teams", asyncHandler(ctrl.getTeams));
-console.log('generateTeams type:', typeof ctrl.generateTeams);
-router.post("/:id/generate-teams", asyncHandler(ctrl.generateTeams));
+// console.log('remove type:', typeof ctrl.remove);
+// router.delete("/:id", asyncHandler(ctrl.remove));
+//
+// console.log('participants type:', typeof ctrl.participants);
+// router.get("/:id/participants", asyncHandler(ctrl.participants));
+// console.log('setParticipants type:', typeof ctrl.setParticipants);
+// router.post("/:id/participants", asyncHandler(ctrl.setParticipants));
+// console.log('getTeams type:', typeof ctrl.getTeams);
+// router.get("/:id/teams", asyncHandler(ctrl.getTeams));
+// console.log('generateTeams type:', typeof ctrl.generateTeams);
+// router.post("/:id/generate-teams", asyncHandler(ctrl.generateTeams));
 
 module.exports = router;
