33c855fb1213cf9b5ffb3ae56a94c1c47320ea81
Tibor Emőd <emod.tibor@gmail.com>
2026-01-24 15:07:11 +0100

    Log more info when http.js asyncHandler calling is failing

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -2,18 +2,18 @@ const router = require("express").Router();
 const ctrl = require("../controllers/matchesController");
 const { asyncHandler } = require("../utils/http");
 
-console.log('generateTeams type:', typeof ctrl.list);
+console.log('list type:', typeof ctrl.list);
 router.get("/", asyncHandler(ctrl.list));
-console.log('generateTeams type:', typeof ctrl.create);
+console.log('create type:', typeof ctrl.create);
 router.post("/", asyncHandler(ctrl.create));
-console.log('generateTeams type:', typeof ctrl.remove);
+console.log('remove type:', typeof ctrl.remove);
 router.delete("/:id", asyncHandler(ctrl.remove));
 
-console.log('generateTeams type:', typeof ctrl.participants);
+console.log('participants type:', typeof ctrl.participants);
 router.get("/:id/participants", asyncHandler(ctrl.participants));
-console.log('generateTeams type:', typeof ctrl.setParticipants);
+console.log('setParticipants type:', typeof ctrl.setParticipants);
 router.post("/:id/participants", asyncHandler(ctrl.setParticipants));
-console.log('generateTeams type:', typeof ctrl.getTeams);
+console.log('getTeams type:', typeof ctrl.getTeams);
 router.get("/:id/teams", asyncHandler(ctrl.getTeams));
 console.log('generateTeams type:', typeof ctrl.generateTeams);
 router.post("/:id/generate-teams", asyncHandler(ctrl.generateTeams));
--- backend/utils/http.js
+++ backend/utils/http.js
@@ -1,2 +1,7 @@
-const asyncHandler = fn => (req,res,next)=>Promise.resolve(fn(req,res,next)).catch(next);
+const asyncHandler = fn => {
+    if (typeof fn !== "function") {
+        throw new TypeError(`asyncHandler expected a function, got ${typeof fn}`);
+    }
+    return (req,res,next)=>Promise.resolve(fn(req,res,next)).catch(next);
+}
 module.exports = { asyncHandler };
