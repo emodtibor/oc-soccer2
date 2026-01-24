a536137701788fe3f5b94eeaf13305dd83edf639
Tibor Emőd <emod.tibor@gmail.com>
2026-01-24 14:32:53 +0100

    Add debug logs for controller method types in matches routes

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -2,14 +2,20 @@ const router = require("express").Router();
 const ctrl = require("../controllers/matchesController");
 const { asyncHandler } = require("../utils/http");
 
+console.log('generateTeams type:', typeof ctrl.list);
 router.get("/", asyncHandler(ctrl.list));
+console.log('generateTeams type:', typeof ctrl.create);
 router.post("/", asyncHandler(ctrl.create));
+console.log('generateTeams type:', typeof ctrl.remove);
 router.delete("/:id", asyncHandler(ctrl.remove));
 
+console.log('generateTeams type:', typeof ctrl.participants);
 router.get("/:id/participants", asyncHandler(ctrl.participants));
+console.log('generateTeams type:', typeof ctrl.setParticipants);
 router.post("/:id/participants", asyncHandler(ctrl.setParticipants));
-
+console.log('generateTeams type:', typeof ctrl.getTeams);
 router.get("/:id/teams", asyncHandler(ctrl.getTeams));
+console.log('generateTeams type:', typeof ctrl.generateTeams);
 router.post("/:id/generate-teams", asyncHandler(ctrl.generateTeams));
 
 module.exports = router;
